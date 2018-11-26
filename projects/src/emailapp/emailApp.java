1fca2576369bbd3ff191f8f407b114c5f136028e
nicholaslane <44882896+nicholaslane@users.noreply.github.com>
2018-11-25 23:23:00 -0500

    this is a test project to create email addresses with 256bit passwords

---
+++ projects/src/emailapp/emailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class emailApp {
+
+	public static void main(String[] args) {
+		email em1 = new email("firstName", "lastName" );
+		System.out.println(em1.showInfo());
+	}
+
+}
